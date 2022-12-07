c3aaf7ba26c317ca15c1bd9b5276b78baacd0737
Oliver Saraguro <andycabrera259@gmail.com>
2022-12-07 10:31:01 -0500

    subiendo

--- ManejoCicloFor/src/paquete01/Ejemplo012.java
+++ ManejoCicloFor/src/paquete01/Ejemplo012.java
@@ -27,6 +27,11 @@ public class Ejemplo012 {
         for (int i = 0; i < 10; i += 3) {
             System.out.printf("%d\n", i);
         }
+        System.out.println("Ciclo con pares del 20 al 30");            
+        
+        for (int i = 20; i <= 30; i += 2) {
+            System.out.printf("%d\n", i);
+        }
         
         
     }
